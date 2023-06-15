1485c674aba6c8745aa0a91982068d8af194eee8
bruxo <diogo.stein@outlook.com.br>
2023-06-15 09:05:18 -0300

    Change url error

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Criar um router para descobrir qual a página atual
+// Rodar função na página certa
+// Criar função fetchAPI que aceita receber um endpoint
+// Criar uma função renderizar os cards
+// Criar um função para renderizar o pokemon na pagina de detalhes
