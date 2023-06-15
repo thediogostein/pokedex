499dcc342925d9908e56f71af2b357f67c2724f7
bruxo <diogo.stein@outlook.com.br>
2023-06-15 09:14:35 -0300

    Create router

--- js/app.js
+++ js/app.js
@@ -3,3 +3,17 @@
 // Criar função fetchAPI que aceita receber um endpoint
 // Criar uma função renderizar os cards
 // Criar um função para renderizar o pokemon na pagina de detalhes
+
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// Router to run functions on different pages
+switch (global.currentPage) {
+  case '/index.html':
+    console.log('Index');
+    break;
+  case '/pokemon.html':
+    console.log('Pokemon');
+    break;
+}
